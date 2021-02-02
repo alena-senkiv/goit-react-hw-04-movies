f6f7a2f7e510ae8057f0ce6c32a9448a479da7b7
Alena <alena.senkiv@gmail.com>
2021-02-02 20:32:43 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import s from './App.module.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <div className={s.container}>goit-react-hw-03-image-finder</div>;
+  }
+}
+
+export default App;
